225c2f1ca438cb09f379da7a3b19c5cd2aa83657
Olimjon <2110192@newuzbekistanuniversity.uz>
2022-12-21 12:12:29 +0500

    feat: create registration and sign in

---
+++ CinemaRes/src/main/java/com/example/cinemares/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package com.example.cinemares;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+    public Connection getConnection(){
+        String databaseName="cinemare";
+
+        String databaseUser="root";
+        String databasePassword="";
+        String url="jdbc:mysql://localhost/"+databaseName;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink= DriverManager.getConnection(url, databaseUser,databasePassword);
+        }catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+        return databaseLink;
+    }
+}
--- CinemaRes/src/main/java/module-info.java
+++ CinemaRes/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.cinemares {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.example.cinemares to javafx.fxml;
