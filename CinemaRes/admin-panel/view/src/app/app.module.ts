68213dfeb4f2ed0755f4ad8e3770d7820aeebff5
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-12-30 01:37:04 +0500

    feat: implement showing movies from database in the view

--- CinemaRes/admin-panel/view/src/app/app.module.ts
+++ CinemaRes/admin-panel/view/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { MovieFormComponent } from './components/movie-form/movie-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieItemComponent
+    MovieItemComponent,
+    MovieFormComponent
   ],
   imports: [
     BrowserModule,
