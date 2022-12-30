57f7843f22d4f2436d1b121a8b8a2d9bb97e81bb
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2022-12-30 23:48:11 +0500

    feat: create modal for creating/updating movies

--- CinemaRes/admin-panel/view/src/app/app.module.ts
+++ CinemaRes/admin-panel/view/src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MAT_DIALOG_DEFAULT_OPTIONS, MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table'
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
-import { MovieFormComponent } from './components/movie-form/movie-form.component';
+import { MovieModalComponent } from './components/movie-modal/movie-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieItemComponent,
-    MovieFormComponent
+    MovieModalComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    CdkTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ CinemaRes/admin-panel/view/src/app/components/movie-modal/movie-modal.component.ts
@@ -0,0 +1,47 @@
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { IMovie } from './../../models/movie.model';
+import { MoviesService } from './../../services/movies.service';
+
+
+
+@Component({
+  selector: 'app-movie-modal',
+  templateUrl: './movie-modal.component.html',
+  styleUrls: ['./movie-modal.component.scss']
+})
+
+export class MovieModalComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<MovieModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: IMovie & {isNew: string},
+    private movieService: MoviesService
+    ) {}
+
+  public movieForm = new FormGroup({
+    name: new FormControl(this.data.name, Validators.required),
+    genre: new FormControl(this.data.genre, Validators.required),
+    description: new FormControl(this.data.description, Validators.required),
+    img: new FormControl(this.data.img, Validators.required),
+    price: new FormControl(this.data.price, Validators.required)
+  })
+
+  public onSubmit() {
+    const movieData: IMovie = {
+      id: this.data.id,
+      name: this.movieForm.getRawValue().name!,
+      genre: this.movieForm.getRawValue().genre!,
+      description: this.movieForm.getRawValue().description!,
+      img: this.movieForm.getRawValue().img!,
+      price: this.movieForm.getRawValue().price!,
+    }
+    this.movieService.updateMovies(movieData);
+    this.dialogRef.close([]);
+  }
+
+}
+
+
