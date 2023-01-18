0b89c4367155898d8e898b0130c8a6df1511cacb
Shahzod Kakhkhorov <shahzod1505@gmail.com>
2023-01-19 04:04:09 +0500

    fix: delete redundant data

--- CinemaRes/admin-panel/view/src/app/main/pages/movie-list/movie-list.component.ts
+++ CinemaRes/admin-panel/view/src/app/main/pages/movie-list/movie-list.component.ts
@@ -19,19 +19,6 @@ export class MovieListComponent implements OnInit, AfterViewInit {
 
   public dataSource: MatTableDataSource<IMovie> = new MatTableDataSource<IMovie>([]);
 
-  public ELEMENT_DATA = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
-
 
   constructor(
                 public moviesService: MoviesService,
@@ -43,7 +30,7 @@ export class MovieListComponent implements OnInit, AfterViewInit {
     this.moviesService.getUsers();
   }
 
-  ngAfterViewInit() { 
+  ngAfterViewInit() {
     this.moviesService.movies.pipe(skip(1)).subscribe((movies: IMovie[]) => {
       this.dataSource = new MatTableDataSource(movies);
       this.dataSource.sort = this.sort;
