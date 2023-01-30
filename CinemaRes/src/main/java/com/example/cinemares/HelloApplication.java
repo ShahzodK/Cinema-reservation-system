17a568ff661372b44b70786fcdbe1eba2ab6bfed
Umida Madiyarova <madiyarova2004@bk.ru>
2023-01-30 12:54:21 +0500

    some changes in design part

--- CinemaRes/src/main/java/com/example/cinemares/HelloApplication.java
+++ CinemaRes/src/main/java/com/example/cinemares/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Payment_1.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Payment.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
 //        stage.setTitle("Hello!");
         stage.setScene(scene);
